c6253948b17f6822df0d21bffb681d499830f409
Eduardo Queiroz <eduhmq@gmail.com>
2025-10-03 13:06:15 -0300

    add remove user with DELETE by id

--- src/main/java/com/edu/workshopmongo/services/UserService.java
+++ src/main/java/com/edu/workshopmongo/services/UserService.java
@@ -32,6 +32,14 @@ public class UserService {
         return repo.insert(obj);
     }
 
+    public void delete(User userObj) {
+        repo.delete(userObj);
+    }
+    public void deleteById(String id) {
+        findById(id);
+        repo.deleteById(id);
+    }
+
     public User fromDTO(UserDTO objDto) {
         return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
