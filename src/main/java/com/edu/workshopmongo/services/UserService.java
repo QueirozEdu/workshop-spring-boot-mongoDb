3dd08b967cc15b3a736dd2f8254c0cc5afebf9e2
Eduardo Queiroz <eduhmq@gmail.com>
2025-10-02 21:43:18 -0300

    add getting a user by id and error handling

--- src/main/java/com/edu/workshopmongo/services/UserService.java
+++ src/main/java/com/edu/workshopmongo/services/UserService.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import com.edu.workshopmongo.domain.User;
 import com.edu.workshopmongo.repository.UserRepository;
+import com.edu.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,5 +19,13 @@ public class UserService {
 
     public List<User> findAll() {
         return repo.findAll();
+    }
+
+    public User findById(String id) {
+        User user = repo.findById(id).orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+        if(user==null) {
+            throw new ObjectNotFoundException("Object not found");
+        }
+        return user;
     }
 }
